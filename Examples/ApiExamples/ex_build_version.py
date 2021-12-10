b349210d73d7cc8fd5b4470b1c8d5f2f91e2ae5b
Alexander Dudin <alexander.dudin@aspose.com>
2021-12-10 20:36:43 +0300

    added ex_build_version

---
+++ Examples/ApiExamples/ex_build_version.py
@@ -0,0 +1,25 @@
+import unittest
+import io
+
+import aspose.words as aw
+import aspose.pydrawing as drawing
+
+from api_example_base import ApiExampleBase, my_dir, artifacts_dir
+
+MY_DIR = my_dir
+ARTIFACTS_DIR = artifacts_dir
+
+class ExBuildVersion(ApiExampleBase):
+
+    def test_print_build_version_info(self):
+
+        #ExStart
+        #ExFor:BuildVersionInfo
+        #ExFor:BuildVersionInfo.Product
+        #ExFor:BuildVersionInfo.Version
+        #ExSummary:Shows how to display information about your installed version of Aspose.Words.
+        print(f"I am currently using {aw.BuildVersionInfo.product}, version number {aw.BuildVersionInfo.version}!")
+        #ExEnd
+
+        self.assertEqual("Aspose.Words for Python via .NET", aw.BuildVersionInfo.product)
+        self.assertRegex(aw.BuildVersionInfo.version, "[0-9]{2}.[0-9]{1,2}.[0-9]")
