bbcec7e4ec4ca7dfde4bd515b25acbb37f441492
Alexander Dudin <alexander.dudin@aspose.com>
2021-12-16 15:33:54 +0300

    added copyright

--- Examples/ApiExamples/ex_load_options.py
+++ Examples/ApiExamples/ex_load_options.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2001-2021 Aspose Pty Ltd. All Rights Reserved.
+#
+# This file is part of Aspose.Words. The source code in this file
+# is only intended as a supplement to the documentation, and is provided
+# "as is", without warranty of any kind, either expressed or implied.
+
 import unittest
 import io
 import os
--- Examples/ApiExamples/ex_moss_doc2pdf.py
+++ Examples/ApiExamples/ex_moss_doc2pdf.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2001-2021 Aspose Pty Ltd. All Rights Reserved.
+#
+# This file is part of Aspose.Words. The source code in this file
+# is only intended as a supplement to the documentation, and is provided
+# "as is", without warranty of any kind, either expressed or implied.
+
 """DOC2PDF document converter for SharePoint.
 Uses Aspose.Words to perform the conversion."""
 
