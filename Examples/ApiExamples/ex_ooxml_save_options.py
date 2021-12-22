5974759ab9eb8d390d9d90172b6919e3aaae13f6
Alexander Dudin <alexander.dudin@aspose.com>
2021-12-22 08:46:11 +0300

    fix typo

--- Examples/ApiExamples/ex_ooxml_save_options.py
+++ Examples/ApiExamples/ex_ooxml_save_options.py
@@ -253,7 +253,7 @@ class ExOoxmlSaveOptions(ApiExampleBase):
                     self.assertLess(prev_file_size, file_size)
 
                     ApiExampleBase.copy_stream(output_file_stream, stream)
-                    self.assertEqual(ffile_signature, ApiExampleBase.dump_array(bytes(stream.getvalue()), 0, 10))
+                    self.assertEqual(file_signature, ApiExampleBase.dump_array(bytes(stream.getvalue()), 0, 10))
 
                     prev_file_size = file_size
 
