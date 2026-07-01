aa111ab0951a308de78714a4243a4eb0a1b6b7e3
Vyacheslav Deryushev <vyacheslav.deryushev@aspose.com>
2026-07-01 14:41:09 +0300

    Created gists for split_documents

--- Examples/DocsExamples/DocsExamples/programming_with_documents/split_documents/split_document.py
+++ Examples/DocsExamples/DocsExamples/programming_with_documents/split_documents/split_document.py
@@ -1,4 +1,4 @@
-import os
+﻿import os
 
 import aspose.words as aw
 from docs_examples_base import DocsExamplesBase, MY_DIR, ARTIFACTS_DIR
@@ -8,7 +8,7 @@ class SplitDocument(DocsExamplesBase):
     def test_by_headings_html(self):
 
         #ExStart:SplitDocumentByHeadings
-        #GistId:c0df00d37081f41a7683339fd7ef66c1
+        #GistId:c5d01a71f65e540e5e77650b846b24cc
         doc = aw.Document(MY_DIR + "Rendering.docx")
 
         options = aw.saving.HtmlSaveOptions()
@@ -24,7 +24,7 @@ class SplitDocument(DocsExamplesBase):
         doc = aw.Document(MY_DIR + "Rendering.docx")
 
         #ExStart:SplitDocumentBySectionsHtml
-        #GistId:6759a1a6b7f448798751d54922a8efcb
+        #GistId:fec4c47291463c8d8d8a6684ffb23d64
         options = aw.saving.HtmlSaveOptions()
         options.document_split_criteria = aw.saving.DocumentSplitCriteria.SECTION_BREAK
         #ExEnd:SplitDocumentBySectionsHtml
@@ -34,7 +34,7 @@ class SplitDocument(DocsExamplesBase):
     def test_by_sections(self):
 
         #ExStart:SplitDocumentBySections
-        #GistId:6759a1a6b7f448798751d54922a8efcb
+        #GistId:fec4c47291463c8d8d8a6684ffb23d64
         doc = aw.Document(MY_DIR + "Big document.docx")
 
         for i in range(doc.sections.count):
@@ -55,7 +55,7 @@ class SplitDocument(DocsExamplesBase):
     def test_page_by_page(self):
 
         #ExStart:SplitDocumentPageByPage
-        #GistId:6759a1a6b7f448798751d54922a8efcb
+        #GistId:fec4c47291463c8d8d8a6684ffb23d64
         doc = aw.Document(MY_DIR + "Big document.docx")
 
         page_count = doc.page_count
@@ -70,7 +70,7 @@ class SplitDocument(DocsExamplesBase):
         self.merge_documents()
 
     #ExStart:MergeSplitDocuments
-    #GistId:6759a1a6b7f448798751d54922a8efcb
+    #GistId:fec4c47291463c8d8d8a6684ffb23d64
     @staticmethod
     def merge_documents():
 
@@ -104,7 +104,7 @@ class SplitDocument(DocsExamplesBase):
     def test_by_page_range(self):
 
         #ExStart:SplitDocumentByPageRange
-        #GistId:6759a1a6b7f448798751d54922a8efcb
+        #GistId:fec4c47291463c8d8d8a6684ffb23d64
         doc = aw.Document(MY_DIR + "Big document.docx")
 
         # Get part of the document.
