e0b81784f17ef785df1c112b0aa4280a477c6db3
Dmitry Kotov <ya.kotovde@yandex.ru>
2026-07-08 18:29:55 +0700

    added examples 26.7

--- Examples/ApiExamples/ex_lists.py
+++ Examples/ApiExamples/ex_lists.py
@@ -810,3 +810,23 @@ class ExLists(ApiExampleBase):
         builder.writeln('Item 2')
         doc.save(file_name=ARTIFACTS_DIR + 'Lists.AddSingleLevelList.docx')
         #ExEnd:AddSingleLevelList
+
+    def test_remove_tab_stop_from_list_level(self):
+        #ExStart:RemoveTabStopFromListLevel
+        #ExFor:ListLevel.remove_tab_stop()
+        #ExSummary:Shows how to clear the list level tab stop.
+        doc = aw.Document()
+        builder = aw.DocumentBuilder(doc=doc)
+        # Create a list with default formatting.
+        builder.list_format.apply_number_default()
+        builder.writeln("Numbered list item 1")
+        builder.writeln("Numbered list item 2")
+        # Get the list level and remove its tab stop.
+        list_level = builder.list_format.list_level
+        list_level.remove_tab_stop()
+        doc.save(file_name=ARTIFACTS_DIR + "Paragraph.RemoveTabStopFromListLevel.docx")
+        #ExEnd:RemoveTabStopFromListLevel
+        # Verify the tab stop was removed.
+        doc = aw.Document(file_name=ARTIFACTS_DIR + "Paragraph.RemoveTabStopFromListLevel.docx")
+        effective_list_level = doc.first_section.body.first_paragraph.list_format.list_level
+        self.assertIsNotNone(effective_list_level)
