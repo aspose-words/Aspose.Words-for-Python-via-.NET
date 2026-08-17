a79af8d7a1949c58706529c1e758f6ec603811d3
Vyacheslav Deryushev <vyacheslav.deryushev@aspose.com>
2026-08-17 10:52:31 +0300

    Added missed examples

---
+++ Examples/DocsExamples/DocsExamples/programming_with_documents/enable_open_type_features.py
@@ -0,0 +1,12 @@
+﻿import aspose.words as aw
+from docs_examples_base import DocsExamplesBase, MY_DIR, ARTIFACTS_DIR
+
+class EnableOpenTypeFeatures(DocsExamplesBase):
+
+    def test_open_type_features(self):
+        #ExStart:OpenTypeFeatures
+        #GistId:b5ab3801a7643f50529361fb177f61f5
+        doc = aw.Document(file_name=MY_DIR + "OpenType text shaping.docx")
+        doc.layout_options.enable_text_shaping = True
+        doc.save(file_name=ARTIFACTS_DIR + 'EnableOpenTypeFeatures.EnableOpenTypeFeatures.pdf')
+        #ExEnd:OpenTypeFeatures
